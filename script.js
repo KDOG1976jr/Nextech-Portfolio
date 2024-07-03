08f9c7b20c7d68d89f668e352aea091a403a5a5c
Keith Owens Jr. <owenskei000@warren.k12.in.us>
2024-07-03 07:29:08 -0400

    Final

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const nextPageBtn = document.getElementById('nextPageBtn');
+    const previousPageBtn = document.getElementById('previousPageBtn');
+
+    if (nextPageBtn) {
+        nextPageBtn.addEventListener('click', () => {
+            document.querySelector('.container').classList.add('fade-out');
+            setTimeout(() => {
+                window.location.href = '/MainMenu/mainMenu.html';
+            }, 1000); 
+        });
+    }
+
+    if (previousPageBtn) {
+        previousPageBtn.addEventListener('click', () => {
+            document.querySelector('.container').classList.add('fade-out');
+            setTimeout(() => {
+                window.location.href = 'index.html';
+            }, 1000); 
+        });
+    }
+});
